cd25bf73eab988a7fdd5e23ff13c0d2226f0f9c4
Rahul Vig <rahvee51395@gmail.com>
2019-04-23 21:51:47 -0400

    edited schema.sql tables

--- db/schema.sql
+++ db/schema.sql
@@ -1,34 +1,40 @@
-###
-Schema
-
+DROP DATABASE IF EXISTS greenups_db;
 CREATE DATABASE greenups_db;
 
 USE greenups_db;
 
-CREATE TABLE events
+CREATE TABLE categories
 (
-    id INT NOT NULL
-    AUTO_INCREMENT,
+    category_id INT
+    AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    category_name VARCHAR
+    (255) NOT NULL
+)
+ENGINE=INNODB;
+
+    CREATE TABLE events
+    (
+        id INT NOT NULL
+        AUTO_INCREMENT,
 	event_name VARCHAR
-    (255) NOT NULL,
+        (255) NOT NULL,
     event_date_time DATETIME NOT NULL,
     event_location VARCHAR
-    (255) NOT NULL,
+        (255) NOT NULL,
     event_created_by VARCHAR
-    (255) NOT NULL,
+        (255) NOT NULL,
     number_of_participants INT
-    (10)
+        (10),
 	PRIMARY KEY
-    (id),
+        (id),
+    cat_id INT NOT NULL,
     FOREIGN KEY
-    (category_id) REFERENCES categories
-    (category_id)
-);
+    FK_CAT
+        (cat_id) REFERENCES categories
+        (category_id)
+    ON
+        UPDATE CASCADE
+   ON
+        DELETE RESTRICT
+) ENGINE=INNODB;
 
-    CREATE TABLE categories
-    (
-        category_id INT NOT NULL
-        AUTO_INCREMENT,
-	category_name VARCHAR
-        (255) NOT NULL
-);

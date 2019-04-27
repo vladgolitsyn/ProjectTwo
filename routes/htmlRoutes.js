dc09ddb5d6aa80d13b9b92806d2f68f02387ba11
Vlad Golitsyn <vladgolitsyn@gmail.com>
2019-04-27 00:41:19 -0400

    added events.handlebars and events.js

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -2,4 +2,8 @@ module.exports = function(app) {
   app.get("/", function(req, res) {
     res.render("index");
   });
+
+  app.get("/events", function(req, res) {
+    res.render("events");
+  });
 };

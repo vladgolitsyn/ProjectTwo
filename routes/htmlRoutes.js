e373f44ef4615e53db17f6f09d52afb86d288ff8
Rahul Vig <rahvee51395@gmail.com>
2019-04-30 18:58:48 -0400

    events

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,16 +3,15 @@ module.exports = function(app) {
     res.render("index");
   });
 
-<<<<<<< HEAD
   app.get("/events", function(req, res) {
     res.render("events");
-=======
+  });
+
   app.get("/register", function(req, res) {
     res.render("register");
   });
 
   app.get("/login", function(req, res) {
     res.render("login");
->>>>>>> 753b83bf514478ef70483ca327fda0efc5741dbc
   });
 };

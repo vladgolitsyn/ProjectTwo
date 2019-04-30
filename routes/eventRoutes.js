9f37a98b617cf00bf7490c95b8ad0055f72707ac
Vlad Golitsyn <vladgolitsyn@gmail.com>
2019-04-30 18:42:35 -0400

    Fix html routes to prevent arg type error

--- routes/eventRoutes.js
+++ routes/eventRoutes.js
@@ -8,32 +8,27 @@ const router = express.Router();
 // Talk to the model
 // const VAR1 = require(path.join(__dirname, "..", "models", "---.js"));
 
-
 /****************************************************************************    
     Set up the routes
 *****************************************************************************/
 
-
 //Get all events from DB:
-app.get("/events", function (req, res) {
-    db.Post.findAll({})
-        .then(function (dbPost) {
-            res.json(dbPost);
-        });
+app.get("/events", function(req, res) {
+  db.Post.findAll({}).then(function(dbPost) {
+    res.json(dbPost);
+  });
 });
 
 //Get route for returning posts of a specific category  **Nice to have?**
-app.get("/events/:category", function (req, res) {
-    db.Post.findAll({
-        where: {
-            category: req.params.category
-        }
-    })
-        .then(function (dbPost) {
-            res.json(dbPost);
-        });
+app.get("/events/:category", function(req, res) {
+  db.Post.findAll({
+    where: {
+      category: req.params.category
+    }
+  }).then(function(dbPost) {
+    res.json(dbPost);
+  });
 });
-
 
 // Get route for retrieving a single post (by category)  **Really nice to have???**
 // app.get("/events/:id", function (req, res) {
@@ -48,44 +43,40 @@ app.get("/events/:category", function (req, res) {
 // });
 
 // POST route for saving a new event
-app.post("/events", function (req, res) {
-    console.log(req.body);
-    db.Post.create({
-        name: req.body.name,
-        date: req.body.date,
-        location: req.body.location,
-        description: req.body.description,
-        category: req.body.category, //FYI this is not in events.js file
-        participants: req.body.participants
-    })
-        .then(function (dbPost) {
-            res.json(dbPost);
-        });
+app.post("/events", function(req, res) {
+  console.log(req.body);
+  db.Post.create({
+    name: req.body.name,
+    date: req.body.date,
+    location: req.body.location,
+    description: req.body.description,
+    category: req.body.category, //FYI this is not in events.js file
+    participants: req.body.participants
+  }).then(function(dbPost) {
+    res.json(dbPost);
+  });
 });
 
 // DELETE route for deleting events
-app.delete("/events/:id", function (req, res) {
-    db.Post.destroy({
-        where: {
-            id: req.params.id
-        }
-    })
-        .then(function (dbPost) {
-            res.json(dbPost);
-        });
+app.delete("/events/:id", function(req, res) {
+  db.Post.destroy({
+    where: {
+      id: req.params.id
+    }
+  }).then(function(dbPost) {
+    res.json(dbPost);
+  });
 });
 
 // PUT route for updating events
-app.put("/events", function (req, res) {
-    db.Post.update(req.body,
-        {
-            where: {
-                id: req.body.id
-            }
-        })
-        .then(function (dbPost) {
-            res.json(dbPost);
-        });
+app.put("/events", function(req, res) {
+  db.Post.update(req.body, {
+    where: {
+      id: req.body.id
+    }
+  }).then(function(dbPost) {
+    res.json(dbPost);
+  });
 });
 
 module.exports = router;
